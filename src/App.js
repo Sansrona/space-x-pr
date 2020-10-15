7c531991d13ea6f3a297cf316c4170af868d23b2
Sansrona <kingJaM00@yandex.ru>
2020-10-15 10:50:43 +0600

    day 1 added components Features/Details/Footer/Calendar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/header/Header";
+import Main from './components/main/Main';
+import Features from './components/features/Features';
+import Footer from './components/footer/Footer';
+import Calendar from './components/calendar/Calendar';
+import Details from './components/details/Details';
+
+import './style.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <React.Fragment>
+  <Header />
+  <Main />
+  <Features />
+  <Footer />
+ 
+
+
+</React.Fragment>
+    
+    );
 }
 
 export default App;
